924dce93230c5279b5713e342c1a9e995e8e88db
Dillon Jeffers <dillon_jeffers@fastmail.com>
2020-03-24 05:26:49 +0000

    specify the asynchronous and the synchronous versions

--- erlang/translate.erl
+++ erlang/translate.erl
@@ -1,14 +1,27 @@
 -module(translate).
--export([loop/0]).
-loop() ->
+-export([loop_async/0, loop_sync/0]).
+loop_async() ->
+    receive
+        "casa" ->
+            io:format("house~n"),
+            loop_async();
+        "blanca" ->
+            io:format("white~n"),
+            loop_async();
+        _ ->
+            io:format("I don't understand.~n"),
+            loop_async()
+    end.
+
+loop_sync() ->
     receive
         {Pid, "casa"} ->
             Pid ! "house",
-            loop();
+            loop_sync();
         {Pid, "blanca"} ->
             Pid ! "white",
-            loop();
+            loop_sync();
         {Pid, _} ->
             Pid ! "I don't understand.",
-            loop()
-end.
+            loop_sync()
+    end.
